5b650c977124997c1bc6694e8329933e99f9e5ca
mike <apolloproductionsdj@gmail.com>
2021-07-13 10:58:41 -0700

    included functionality to add an image upload to firebase storage, had to change rules and merge previous posts information

--- components/InputBox.js
+++ components/InputBox.js
@@ -2,13 +2,16 @@ import { session, useSession } from "next-auth/client";
 import Image from "next/image";
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
-import { useRef } from "react";
-import { db } from "../firebase";
+import { useRef, useState } from "react";
+import { db, storage } from "../firebase";
 import firebase from 'firebase';
 
 function InputBox() {
     const [session] = useSession();
     const inputRef = useRef(null);
+    const filepickerRef = useRef(null);
+    const [imageToPost, setImageToPost] = useState(null);
+
 
     const sendPost = (e) => {
         e.preventDefault();
@@ -20,9 +23,41 @@ function InputBox() {
             email: session.user.email,
             image: session.user.image,
             timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        }).then(doc => {
+            if (imageToPost) {
+                const uploadTask = storage.ref(`posts/${doc.id}`).putString(imageToPost,
+                    'data_url')
+
+                removeImage();
+
+                uploadTask.on('state_change', null, error => console.log(error),
+                    () => {
+                        // When the upload completes
+                        storage.ref('posts').child(doc.id).getDownloadURL().then(url => {
+                            db.collection('posts').doc(doc.id).set({
+                                postImage: url
+                            }, { merge: true })
+                        })
+                    })
+            }
         })
 
         inputRef.current.value = "";
+    };
+
+    const addImageToPost = (e) => {
+        const reader = new FileReader();
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0])
+        }
+
+        reader.onload = (readerEvent) => {
+            setImageToPost(readerEvent.target.result)
+        }
+    };
+
+    const removeImage = () => {
+        setImageToPost(null);
     };
 
     return (
@@ -44,6 +79,16 @@ function InputBox() {
                     />
                     <button hidden type="submit" onClick={sendPost}>Submit</button>
                 </form>
+
+                {imageToPost && (
+                    <div onClick={removeImage} className="flex flex-col filter hover:bright-110 transition duration-150 
+                    transform hover:scale-105 cursor-pointer">
+                        <img className="h-10 object-contain" src={imageToPost} alt="" />
+                        <p className="text-xs text-red-500 text-center">
+                            Remove
+                        </p>
+                    </div>
+                )}
             </div>
             <div className="flex justify-evenly p-3 border-t">
                 <div className="inputIcon">
@@ -54,11 +99,12 @@ function InputBox() {
                     </p>
 
                 </div>
-                <div className="inputIcon">
+                <div onClick={() => filepickerRef.current.click()} className="inputIcon">
                     <CameraIcon className="h-7 text-green-400" />
                     <p className="text-xs sm:text-sm xl:text-base">
                         Photo/Video
                     </p>
+                    <input ref={filepickerRef} onChange={addImageToPost} type="file" hidden />
                 </div>
                 <div className="inputIcon">
                     <EmojiHappyIcon className="h-7 text-yellow-300" />
